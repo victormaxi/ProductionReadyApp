e13bf02c32a127276de53f835b2339b3cd3fe3ae
kelechi onu <maximillianonu@gmail.com>
2021-11-04 23:40:44 +0100

    intial commit

---
+++ ProductionReadyAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProductionReadyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+       [HttpGet]
+       public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message");
+            _logger.LogDebug("Here is debug message");
+            _logger.LogWarn("Here is warn message");
+            _logger.LogError("Here is Error message");
+
+            return new string[] { "value1", "value2" };
+        }
+
+     
+    }
+}
